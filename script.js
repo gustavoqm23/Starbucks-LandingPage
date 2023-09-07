cd592ff758cb1422bda86c9140112de926b0b830
Gustavo Monte <gustavoqmonte2004@gmail.com>
2023-09-06 21:59:30 -0300

    Responsive Starbucks Landing Page

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+//Troca de imagem ao clicar
 function imgSlider(anything){
     document.querySelector('.starbucks').src = anything;
 }
 
+//Muda cor do círculo
 function changeCircleColor(color) {
     const circle = document.querySelector('.circle');
     circle.style.background = color;
+}
+
+//Abre e Fecha Menu
+function toggleMenu() {
+    var menuToggle = document.querySelector('.toggle');
+    var navbar = document.querySelector('.navbar')
+    menuToggle.classList.toggle('active');
+    navbar.classList.toggle('active');
 }
